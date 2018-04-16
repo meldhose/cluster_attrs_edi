ad23af0710b2eeb698968598eb1ec7e4adb8a388
Meera George <meera@cs.wisc.edu>
2018-04-16 10:18:39 -0500

    Update __init__.py

--- cluster_attrs_edi/__init__.py
+++ cluster_attrs_edi/__init__.py
@@ -4,16 +4,12 @@ import sys
 if sys.version_info > (3, 0):
     import importlib
     importlib.reload(sys)
-
-    from .cluster_attrs_edi import *
-
+    
     from .get_attr_names import *
     from .cluster_attr_names import *
 else:
     reload(sys)
     sys.setdefaultencoding("UTF-8")
-
-    from cluster_attrs_edi import *
 
     from get_attr_names import *
     from cluster_attr_names import *
